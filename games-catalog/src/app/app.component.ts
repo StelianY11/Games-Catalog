995fc4897d44bb09329eadb3a2a0e863b88b15ea
StelianY11 <134134591+StelianY11@users.noreply.github.com>
2024-11-28 18:55:57 +0200

    Add main pages and style them

--- games-catalog/src/app/app.component.ts
+++ games-catalog/src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { GamesComponent } from './games/games.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, LoginComponent, RegisterComponent, PageNotFoundComponent, GamesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
