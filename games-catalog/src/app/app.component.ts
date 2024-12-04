8234cc9c1b8e73456adb6f0536ed99cc93281805
StelianY11 <134134591+StelianY11@users.noreply.github.com>
2024-12-04 14:59:30 +0200

    Add all of the pages and make some adjustments

--- games-catalog/src/app/app.component.ts
+++ games-catalog/src/app/app.component.ts
@@ -2,16 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './user/login/login.component';
-import { RegisterComponent } from './user/register/register.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { GamesComponent } from './game-resources/games/games.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, LoginComponent, RegisterComponent, PageNotFoundComponent, GamesComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- games-catalog/src/app/app.routes.ts
+++ games-catalog/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { GamesComponent } from './game-resources/game/games.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AddGameComponent } from './game-resources/add-game/add-game.component';
+import { GameDetailsComponent } from './game-resources/games-details/game-details.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'profile', component: ProfileComponent },
+    {
+        path: 'games', children: [
+            { path: "", component: GamesComponent },
+            { path: ":gameId", component: GameDetailsComponent }
+        ]
+    },
+    { path: "game-details", component: GameDetailsComponent },
+    { path: "about", component: AboutComponent },
+    { path: "add-game", component: AddGameComponent, /*canActivate: [AuthGuard] */},
+    { path: "404", component: PageNotFoundComponent },
+    { path: '**', redirectTo: "/404" },
+];
